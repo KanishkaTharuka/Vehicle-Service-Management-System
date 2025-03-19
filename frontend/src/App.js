53b5d00cc7922f22b16d0c5a2d8ddcb4ac37f780
KanishkaTharuka <kanishkatharuka500@gmail.com>
2025-03-20 02:31:36 +0530

    create folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        {/* Routes */}
+        
+          
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
