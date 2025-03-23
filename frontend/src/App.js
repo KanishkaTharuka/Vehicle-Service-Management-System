7002245eaa337374a97f07fc031ac148f926460f
KanishkaTharuka <kanishkatharuka500@gmail.com>
2025-03-23 15:53:24 +0530

    update my txt file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './BreakdownServiceManagement/breakdownHome';
+import BreakdownView from './BreakdownServiceManagement/breakdownview';
+import BreakdownEdit from './BreakdownServiceManagement/breakdownEdit';
 
 function App() {
   return (
