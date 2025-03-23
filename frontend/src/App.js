49293c7458df56c91e4c627c103519e1f695a218
KanishkaTharuka <kanishkatharuka500@gmail.com>
2025-03-23 15:30:49 +0530

    Added  Heder & fotter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Homepage from './home/homepage';
+import Navbar from './home/Header';
+import Footer from './home/Footer';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-        {/* Routes */}
+      <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
         
           
         
